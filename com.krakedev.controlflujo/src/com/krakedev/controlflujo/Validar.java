688ac55032b109041d284a2ced01813d93d5f329
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-11-06 10:14:25 -0500

    sintaxis del if --else

--- com.krakedev.controlflujo/src/com/krakedev/controlflujo/Validar.java
+++ com.krakedev.controlflujo/src/com/krakedev/controlflujo/Validar.java
@@ -2,8 +2,10 @@ package com.krakedev.controlflujo;
 
 public class Validar {
 	public void ValidarEdad(int edad) {
-		if(edad>=18) {
+		if(edad>=18) { // sentencia if
 			System.out.println("Es mayor de edad");
+		}else {
+			System.out.println("Es menor de edad");
 		}
 	}
 
