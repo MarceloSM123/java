2986c48d914b76cfebe6dcf290a98af115b2e16b
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-11-10 10:05:24 -0500

    Ejercicio 10/ reto 18

--- Evaluacion1MarceloSalcedo/src/com/clearminds/maquina/MaquinaDulces.java
+++ Evaluacion1MarceloSalcedo/src/com/clearminds/maquina/MaquinaDulces.java
@@ -3,6 +3,7 @@ package com.clearminds.maquina;
 import java.util.ArrayList;
 
 import com.clearminds.componentes.Celda;
+import com.clearminds.componentes.Producto;
 
 public class MaquinaDulces {
 	private ArrayList<Celda> celdas=new ArrayList<Celda>();
@@ -34,7 +35,7 @@ public class MaquinaDulces {
 	
 	public Celda buscarCelda (String codigo) {
 		Celda c1;
-		boolean a=false;
+		//boolean a=false;
 		for(int i=0;i<celdas.size();i++) {
 			 c1=celdas.get(i);
 		if(c1.getCodigo().equals(codigo)) {
@@ -44,4 +45,76 @@ public class MaquinaDulces {
 		}
 		return null;
 	}
-}
+	
+	public void cargarProducto(Producto p1, String codigo, int cantidadItems) {
+		Celda celdaRecuperada=buscarCelda(codigo);
+		celdaRecuperada.ingresarProducto(p1,cantidadItems);
+	}
+	
+	public void mostrarProductos() {
+		Celda c1;
+		for(int i=0;i<celdas.size();i++) {
+			 c1=celdas.get(i);
+		if(c1.getProducto()!=null) {
+			System.out.println("Codigo: "+c1.getCodigo());
+			System.out.println("Stock: "+c1.getStock());
+			System.out.println("Nombre: "+c1.getProducto().getCodigo());
+			System.out.println("Precio: "+c1.getProducto().getPrecio());
+			
+		}else {
+			System.out.println("Codigo: "+c1.getCodigo());
+			System.out.println("Stock: "+c1.getStock());
+			System.out.println("Sin producto asignado");
+		}
+		}
+		
+		}
+	
+	public Producto buscarProductoEnCelda(String codigo){
+		Celda c1;
+		//boolean a=false;
+		for(int i=0;i<celdas.size();i++) {
+			 c1=celdas.get(i);
+		if(c1.getCodigo().equals(codigo)) {
+			return c1.getProducto();
+		}
+		
+		}
+		return null;
+	}
+	
+	public double consultarPrecio(String codigo) {
+		Celda c1;
+		//boolean a=false;
+		for(int i=0;i<celdas.size();i++) {
+			 c1=celdas.get(i);
+		if(c1.getCodigo().equals(codigo)) {
+			return c1.getProducto().getPrecio();
+		}
+		
+		}
+		return 0.0;
+	}
+	
+	public Celda buscarCeldaProducto(String codigo){
+		Celda c1;
+		//boolean a=false;
+		for(int i=0;i<celdas.size();i++) {
+			 c1=celdas.get(i);
+	//	if(c1.getProducto().getCodigo().equals(codigo)) {
+			 if(c1.getProducto()!=null) {
+			 if(codigo.equals(c1.getProducto().getCodigo())) {
+				 return c1;
+		}}
+		
+		}
+		return null;
+	}
+	
+	public void incrementarProductos(String codigo, int cantidadItems) {
+		Celda celdaEncontrada=buscarCeldaProducto(codigo);
+	    celdaEncontrada.setStock(celdaEncontrada.getStock()+cantidadItems);
+	}
+	
+	}
+//}
