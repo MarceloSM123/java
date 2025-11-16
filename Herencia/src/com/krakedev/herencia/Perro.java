c4bd9b9a04e7992f8105d44321e0b687778c3d82
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-11-16 18:54:11 -0500

    Todas las clases heredan metodos de object

--- Herencia/src/com/krakedev/herencia/Perro.java
+++ Herencia/src/com/krakedev/herencia/Perro.java
@@ -1,5 +1,7 @@
 package com.krakedev.herencia;
 
 public class Perro extends Animal{// perro hereda de animal
-
+	public void ladrar() {
+		System.out.println("perro ladrando");
+	}
 }
--- Herencia/src/com/krakedev/herencia/TestHerencia.java
+++ Herencia/src/com/krakedev/herencia/TestHerencia.java
@@ -5,7 +5,11 @@ public class TestHerencia {
 	public static void main(String[] args) {
 		Perro p =new Perro();
 		p.dormir();
-
+		Animal a =new Animal();
+		System.out.println(a.hashCode());// es un metodo de object 
+		// animal hereda este metodo de object 
+		// object es una clase padre para todas las clases
+		// al colocar p. se depliegan metodos de la clase objeto, clase perro, clase animal
 	}
 
 }
