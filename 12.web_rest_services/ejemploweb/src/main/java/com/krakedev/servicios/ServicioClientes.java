77f65693fbd78aa5630552f53404ffa13af927e6
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-11-19 18:36:09 -0500

    reto 36 completo

--- 12.web_rest_services/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
+++ 12.web_rest_services/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
@@ -3,9 +3,9 @@ package com.krakedev.servicios;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-@Path("cliente")// como se encadena la direccion
+@Path("customers")// como se encadena la direccion
 public class ServicioClientes {
-	@Path("metodo1") // se encadena la direccion 
+	@Path("m1") // se encadena la direccion 
 	@GET
 	public String saludar() {
 		return "HOLA MUNDO REST WEB SERVICES";
