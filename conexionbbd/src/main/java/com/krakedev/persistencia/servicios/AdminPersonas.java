08f909f4c0e4fe322514e9565e5b090d15ac5a84
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-11-18 23:12:29 -0500

    implementacion de metodo actualizar--ejercicio 1 reto 33

--- conexionbbd/src/main/java/com/krakedev/persistencia/servicios/AdminPersonas.java
+++ conexionbbd/src/main/java/com/krakedev/persistencia/servicios/AdminPersonas.java
@@ -5,11 +5,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.krakedev.persistencia.utils.ConexionBDD;
 import com.krakedev.pesistencia.entidades.Persona;
 
 public class AdminPersonas {
-	public static void insertar(Persona persona) {	
+	private static final Logger LOGGER=LogManager.getLogger(AdminPersonas.class);
+	
+	public static void insertar(Persona persona){// throws Exception{	
 			Connection con=null;
 			PreparedStatement ps=null;
 		try {
@@ -27,19 +32,61 @@ public class AdminPersonas {
 						ps.executeUpdate();
 		}catch(Exception e) {
 			//mostrar el error al usuario
-			System.out.println(e.getMessage());
+			//System.out.println(e.getMessage());
 			//loggear el error
+			LOGGER.error("Error insertar",e);
 			e.printStackTrace();
+			//throw e;
 		}finally {
 			//cerrar conexion 
 			try {
 				con.close();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.out.println("ERROR INFRAESTRUCTURA");
+				// e.printStackTrace(); en lugar va un logger
+				LOGGER.error("\"Error con la base de datos\"", e);
+			//sino usa logger usar esto	throw new Exception("Error con la base de datos");
+				//System.out.println("ERROR INFRAESTRUCTURA");
 			}
 		}
 	}
+	
+	public static void actualizar(Persona persona) {
+		Connection con=null;
+		PreparedStatement ps=null;
+	try {
+		 con=ConexionBDD.conectar();// SI SE DECLARA DENTRO DEL TRY SOLO SIRVE EN EL TRY 
+		//System.out.println("INSERTAR");
+		 ps=con.prepareStatement("UPDATE transacciones SET numero_cuenta=?, monto=?, tipo=?, fecha=?, hora=? WHERE codigo=?");
+				//ingresamos los datos
+		 ps.setString(1, persona.getNumero_cuenta());
+	        ps.setBigDecimal(2, persona.getMonto());
+	        ps.setString(3, persona.getTipo());
+	        ps.setDate(4, persona.getFecha());
+	        ps.setTime(5, persona.getHora());
+	        LOGGER.debug(persona.getHora());
+	        ps.setInt(6, persona.getCodigo()); 
+	        ps.executeUpdate();
+	}catch(Exception e) {
+		//mostrar el error al usuario
+		//System.out.println(e.getMessage());
+		//loggear el error
+		LOGGER.error("Error al actualizar",e);
+		e.printStackTrace();
+		//throw e;
+	}finally {
+		//cerrar conexion 
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace(); en lugar va un logger
+			LOGGER.error("Error cerrando conexion", e);
+		//sino usa logger usar esto	throw new Exception("Error con la base de datos");
+			//System.out.println("ERROR INFRAESTRUCTURA");
+		}
+	}
+	}
+	
 	}
 
--- conexionbbd/src/main/java/com/krakedev/persistencia/test/TestAdminPersona.java
+++ conexionbbd/src/main/java/com/krakedev/persistencia/test/TestAdminPersona.java
@@ -10,8 +10,9 @@ public class TestAdminPersona {
 	public static void main(String[] args) {
 	//	values (1,'12345',12.12,'D','12/01/1999','12:12:12')
 
-		AdminPersonas.insertar(new Persona(1,"12345",12.12,"D","12/01/1999","12:12:12"));
-		AdminPersonas.insertar(new Persona(2,"22222",10000.12,"D","18/11/2025","22:04:12"));
+		//AdminPersonas.insertar(new Persona(1,"12345",12.12,"D","12/01/1999","12:12:12"));
+	//	AdminPersonas.insertar(new Persona(2,"22222",10000.12,"D","18/11/2025","22:04:12"));
+		AdminPersonas.actualizar(new Persona(2,"22222",999999.12,"D","18/11/2025","00:00:00"));
 	}
 
 }
