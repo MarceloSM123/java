55421c1582538bf74e33aaeacf1a46d76396388c
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-11-18 19:03:02 -0500

    estructura try/catch/finally

--- excepciones/src/main/java/com/krakedev/excepciones/Ejercicio1.java
+++ excepciones/src/main/java/com/krakedev/excepciones/Ejercicio1.java
@@ -3,12 +3,19 @@ package com.krakedev.excepciones;
 public class Ejercicio1 {
 	public static void main(String[] args) {
 		System.out.println("inicio");
-		String a=null;
+		// String a=null; // genera una excepcion que activa al bloque catch
+		String a="xxxss";
+		//si no hay excepciones entra al bloque try
 		try {
 			a.substring(3);
 			System.out.println("fin");
+		//si sucede una excepcion salta al bloque catch
+		// impide que el programa se detenga por escepciones 
 		}catch(Exception ex){
 		System.out.println("Entra en el catch");
+		}finally {
+			System.out.println("ingresa al finally");
+			//el bloque se ejecuta se ejecute o no el try o el catch
 		}
 		System.out.println("fuera del catch");
 	}
