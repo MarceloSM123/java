2e86f8ec88bf19f9bcd35ede3105f89636dcbd73
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-11-19 20:47:43 -0500

    devolver oobjetos

--- 12.web_rest_services/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
+++ 12.web_rest_services/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
@@ -1,7 +1,12 @@
 package com.krakedev.servicios;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.krakedev.entidades.Cliente;
 
 @Path("customers")// como se encadena la direccion
 public class ServicioClientes {
@@ -9,5 +14,12 @@ public class ServicioClientes {
 	@GET
 	public String saludar() {
 		return "HOLA MUNDO REST WEB SERVICES";
+	}
+	@Path("buscar")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Cliente buscar() {
+		Cliente cliente = new Cliente("173333","Marcelo");
+		return cliente;
 	}
 }
